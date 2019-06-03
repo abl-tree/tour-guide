58996200df48805726425ef6e93fd58e1e308862
Allen Beciera Lamparas <ablamparas@gmail.com>
2019-06-03 16:33:55 +0800

    change password separator

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -102,8 +102,10 @@
                             </div>
                         </div>
 
+                        <div role="separator" class="dropdown-divider"></div>
+
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('New Password') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="new-password">
@@ -117,7 +119,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm New Password') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" autocomplete="new-password">
