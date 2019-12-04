9118c166151d53b2be94cea5b35e9e929c6b17c6
Allen Beciera Lamparas <ablamparas@gmail.com>
2019-12-04 22:52:48 +0800

    ui updates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,90 +1,187 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('subtitle')
-Login
-@endsection
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <title>{{ config('app.name', 'Laravel') }} | @yield('subtitle')</title>
 
-                <div class="card-body">  
-                    @error('unauthorized')
-                        <div class="alert alert-danger" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </div>
-                    @enderror
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    
+    <link
+      href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons"
+      rel="stylesheet"
+    >
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css"
+      rel="stylesheet"
+    >
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/4.4.95/css/materialdesignicons.min.css"
+      rel="stylesheet"
+    >
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address/Username') }}</label>
+    <!-- Icon -->
+    <link rel="icon" href="{{ asset('images/logo.webp') }}">
 
-                            <div class="col-md-6">
-                                <input id="email" type="text" class="form-control @error('email') is-invalid @enderror @error('username') is-invalid @enderror" name="email-username" value="{{ old('email') ? old('email') : old('username') }}" required autofocus>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+    <style>
+        .container {
+            width: 100%!important;
+        }
+        .form-group {
+            margin-bottom: unset!important;
+        }
+        .nav-link {
+            color: white !important;
+        }
+        .navbar-toggler {
+            background-color: white !important;
+        }
 
-                                @error('username')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+    </style>
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-light bg-blue">
+            <div class="container">
+                <!-- Image and text -->
+                <nav class="navbar">
+                    <!-- <a class="navbar-brand" href="{{ url('/') }}">
+                        <img src="{{ asset('images/logo.webp') }}" alt="logo" style="max-height: 50px;">
+                        <span class="d-none d-lg-inline">{{ config('app.name', 'Laravel') }} | Home</span>
+                    </a> -->
+                </nav>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @endguest
+                    </ul>
                 </div>
             </div>
+        </nav>
+
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <img src="{{ asset('images/logo_white.png') }}" alt="logo" style="max-height: 200px;">
+            </div>
         </div>
+
+        <div class="row">
+            <div class="col-md-12 text-center" style="color: white;">
+                <h1>Tour Guides Management Center</h1>
+            </div>
+        </div>
+
+        <main class="py-4" style="padding-top: unset !important;">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        <div class="card">
+                            <div class="card-header">{{ __('Login') }}</div>
+
+                            <div class="card-body">  
+                                @error('unauthorized')
+                                    <div class="alert alert-danger" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </div>
+                                @enderror
+
+                                <form method="POST" action="{{ route('login') }}">
+                                    @csrf
+
+                                    <div class="form-group row">
+                                        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address/Username') }}</label>
+
+                                        <div class="col-md-6">
+                                            <input id="email" type="text" class="form-control @error('email') is-invalid @enderror @error('username') is-invalid @enderror" name="email-username" value="{{ old('email') ? old('email') : old('username') }}" required autofocus>
+
+                                            @error('email')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+
+                                            @error('username')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                                        <div class="col-md-6">
+                                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                            @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <div class="col-md-6 offset-md-4">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                                <label class="form-check-label" for="remember">
+                                                    {{ __('Remember Me') }}
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row mb-0">
+                                        <div class="col-md-8 offset-md-4">
+                                            <button type="submit" class="btn btn-primary">
+                                                {{ __('Login') }}
+                                            </button>
+
+                                            @if (Route::has('password.request'))
+                                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                    {{ __('Forgot Your Password?') }}
+                                                </a>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
     </div>
-</div>
-@endsection
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,89 +1,231 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('subtitle')
-Home
-@endsection
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-@section('content')
-<div class="container">
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item active" aria-current="page">Home</li>
-        </ol>
-    </nav>
+    <title>{{ config('app.name', 'Laravel') }} | @yield('subtitle')</title>
 
-    <div class="card">
-        <div class="card-body">
-            <div class="row text-center">
-                <div class="col-md-6 col-sm-12" style="float: none; margin: 0 auto;">
-                    <a class="btn btn-primary" style="width: 100%" href="/home">Availabilities</a>
-                </div>
-            </div>
-            <div class="row text-center">
-                <div class="col-md-6 col-sm-12" style="float: none; margin: 0 auto;">
-                    <a class="btn btn-primary" style="width: 100%" href="/payment/create">Anticipi & Incassi</a>
-                </div>
-            </div>
-            <div class="row text-center">
-                <div class="col-md-6 col-sm-12" style="float: none; margin: 0 auto;">
-                    <a class="btn btn-primary" style="width: 100%" href="{{ route('articles.index') }}">Articles</a>
-                </div>
-            </div>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
-            @if(Auth::user()->access_levels()->whereHas('info', function($q) {
-            $q->where('code', 'admin');
-            })->first())
-            <div class="row text-center">
-                <div class="col-md-6 col-sm-12" style="float: none; margin: 0 auto;">
-                    <a class="btn btn-primary" style="width: 100%" href="/tours">Tours Listing</a>
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    
+    <link
+      href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons"
+      rel="stylesheet"
+    >
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css"
+      rel="stylesheet"
+    >
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/4.4.95/css/materialdesignicons.min.css"
+      rel="stylesheet"
+    >
+
+    <!-- Icon -->
+    <link rel="icon" href="{{ asset('images/logo.webp') }}">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+
+    <style>
+        .container {
+            width: 100%!important;
+        }
+        .form-group {
+            margin-bottom: unset!important;
+        }
+        .clock-widget span{
+            color: white;
+            font-size: 20px;
+        }
+    </style>
+    <script type="text/javascript"> 
+        function display_c(){
+            var refresh=1000; // Refresh rate in milli seconds
+            mytime=setTimeout('display_ct()',refresh)
+        }
+
+        function display_ct() {
+            var x = new Date()
+            var hours = x.getHours()
+            var minutes = x.getMinutes()
+            var seconds = x.getSeconds()
+            var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+            var day = x.getDate()
+            var month = months[x.getMonth()]
+            var year = x.getFullYear()
+
+            if(hours < 10) {
+                hours = '0' + hours
+            }
+
+            if(minutes < 10) {
+                minutes = '0' + minutes
+            }
+
+            if(seconds < 10) {
+                seconds = '0' + seconds
+            }
+
+            document.getElementById('time').innerHTML = hours + ':' + minutes + ':' + seconds
+            document.getElementById('date').innerHTML = month + ' ' + day + ', ' + year
+            display_c()
+        }
+    </script>
+</head>
+<body onload=display_ct();>
+    <div id="app">
+        <nav class="navbar navbar-dark bg-blue">
+            <div class="container">
+                <!-- Image and text -->
+                <nav class="navbar">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        <img src="{{ asset('images/logo_white.png') }}" alt="logo" style="max-height: 60px;">
+                        <span class="d-none d-lg-inline" style="font-family: serif;">{{ config('app.name', 'Laravel') }} | Home</span>
+                    </a>
+                </nav>
+                <div class="clock-widget">
+                    <span id="time"></span>
+                    <span id="date"></span>
                 </div>
             </div>
-            <div class="row text-center">
-                <div class="col-md-6 col-sm-12" style="float: none; margin: 0 auto;">
-                    <a class="btn btn-primary" style="width: 100%" href="/tourcalendar">Calendars</a>
+        </nav>
+
+        <main class="py-4">
+            <div class="container">    
+                <div class="row menu">
+                    <div class="col-md-3">
+                        <a href="/home">
+                            <div class="card-body availability">
+                                <div class="col-md-12 text-center">
+                                    <span>Availabilities</span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3">
+                        <a href="/payment/create">
+                            <div class="card-body anticipi-incassi">
+                                <div class="col-md-12 text-center">
+                                    <span>Acticipi & Incassi</span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3">
+                        <a href="{{ route('articles.index') }}">
+                            <div class="card-body articles">
+                                <div class="col-md-12 text-center">
+                                    <span>Articles</span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    @if(Auth::user()->access_levels()->whereHas('info', function($q) {
+                    $q->where('code', 'admin');
+                    })->first())
+                    <div class="col-md-3">
+                        <a href="/tours">
+                            <div class="card-body tours-listing">
+                                <div class="col-md-12 text-center">
+                                    <span>Tour Listing</span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3">
+                        <a href="/tourcalendar">
+                            <div class="card-body calendars">
+                                <div class="col-md-12 text-center">
+                                    <span>Calendars</span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3">
+                        <a href="{{ route('tourguide.index') }}">
+                            <div class="card-body tour-guides-listing">
+                                <div class="col-md-12 text-center">
+                                    <span>Tour Guide Listing</span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3">
+                        <a href="/notification">
+                            <div class="card-body notifications">
+                                <div class="col-md-12 text-center">
+                                    <span>Notifications</span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3">
+                        <a href="/statistics">
+                            <div class="card-body economics-statistics">
+                                <div class="col-md-12 text-center">
+                                    <span>Economics & Statistics</span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    @else
+                    <div class="col-md-3">
+                        <a href="/guide/statistics">
+                            <div class="card-body economics-statistics">
+                                <div class="col-md-12 text-center">
+                                    <span>Economics</span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    @endif
+                    <div class="col-md-3">
+                        <a href="/myprofile">
+                            <div class="card-body my-profile">
+                                <div class="col-md-12 text-center">
+                                    <span>My Profile</span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3">
+                        <a href="/profile">
+                            <div class="card-body account-setting">
+                                <div class="col-md-12 text-center">
+                                    <span>Account Setting</span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-3">
+                        <a href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                            <div class="card-body logout">
+                                <div class="col-md-12 text-center">
+                                    <span>Logout</span>
+                                </div>
+                            </div>
+                        </a>
+                        
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                    </div>
                 </div>
             </div>
-            <div class="row text-center">
-                <div class="col-md-6 col-sm-12" style="float: none; margin: 0 auto;">
-                    <a class="btn btn-primary" style="width: 100%" href="/notification">Notifications</a>
-                </div>
-            </div>
-            <div class="row text-center">
-                <div class="col-md-6 col-sm-12" style="float: none; margin: 0 auto;">
-                    <a class="btn btn-primary" style="width: 100%" href="/statistics">Economics & Statistics</a>
-                </div>
-            </div>
-            <div class="row text-center">
-                <div class="col-md-6 col-sm-12" style="float: none; margin: 0 auto;">
-                    <a class="btn btn-primary" style="width: 100%" href="{{ route('tourguide.index') }}">Tour Guides Listing</a>
-                </div>
-            </div>
-        @else
-            <div class="row text-center">
-                <div class="col-md-6 col-sm-12" style="float: none; margin: 0 auto;">
-                    <a class="btn btn-primary" style="width: 100%" href="/guide/statistics">Economics</a>
-                </div>
-            </div>
-        @endif
-            <div class="row text-center">
-                <div class="col-md-6 col-sm-12" style="float: none; margin: 0 auto;">
-                    <a class="btn btn-primary" style="width: 100%" href="/myprofile">My Profile</a>
-                </div>
-            </div>
-            <div class="row text-center">
-                <div class="col-md-6 col-sm-12" style="float: none; margin: 0 auto;">
-                    <a class="btn btn-primary" style="width: 100%" href="/profile">My Account Settings</a>
-                </div>
-            </div>
-            <div class="row text-center">
-                <div class="col-md-6 col-sm-12" style="float: none; margin: 0 auto;">
-                    <a class="btn btn-primary" style="width: 100%" href="{{ route('logout') }}"
-                    onclick="event.preventDefault();
-                    document.getElementById('logout-form').submit();">Logout</a>
-                </div>
-            </div>
-        </div>
-        
+        </main>
     </div>
-</div>
-@endsection
+</body>
+</html>
