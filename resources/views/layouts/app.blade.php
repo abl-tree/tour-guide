2924a09457aac5246a3d487caf0bb7216f40d74b
Allen Beciera Lamparas <ablamparas@gmail.com>
2019-12-04 23:18:41 +0800

    ui update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -98,11 +98,11 @@
                 <div class="clock-widget">
                     <span id="time">Time</span>
                     <span id="date">Date</span>
-                </div>
 
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
